8a7cdde8131f4ee5a9e5760c9f2bf224a134e51a
jjmiranda8 <jmlivingfree@gmail.com>
2022-07-10 16:21:43 -0500

    Add specs and organize files

---
+++ src/pieces/pawn.rb
@@ -0,0 +1,31 @@
+require_relative 'piece'
+
+class Pawn < Piece
+  def initialize(coordinate, color)
+    super(coordinate, color)
+  end
+
+  def possible_moves
+    moves = []
+    moves
+  end
+
+  def at_start
+  end
+
+  def forward
+  end
+
+  def left
+  end
+
+  def right
+  end
+
+  def take_left
+  end
+
+  def take_right
+  end
+end
+
