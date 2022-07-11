d920e752f234d182b91946b92160eb369fc59222
jjmiranda8 <jmlivingfree@gmail.com>
2022-07-10 22:01:28 -0500

    Add logic for queen possible moves

---
+++ src/pieces/piece_helpers.rb
@@ -0,0 +1,17 @@
+def filter_out_of_bounds array_of_moves
+  valid_moves = []
+  array_of_moves.each do |coordinate|
+    x, y = coordinate[0], coordinate[1]
+    if (x > 7 || x < 0) || (y > 7 || y < 0)
+      next
+    else 
+      valid_moves.push(coordinate)
+    end
+  end
+end
+
+def chess_line(to_remove=nil)
+  full_array = [*0..7]
+  full_array.delete to_remove 
+  full_array
+end
--- src/pieces/queen.rb
+++ src/pieces/queen.rb
@@ -1,22 +1,51 @@
 require_relative 'piece'
+require_relative 'piece_helpers'
 
 class Queen < Piece
+  attr_accessor :x, :y
+
   def initialize(coordinate, color)
     super(coordinate, color)
+    @x = coordinate[0]
+    @y = coordinate[1]
   end
 
   def possible_moves
-    moves = []
+    moves = [
+      *vertical_moves,
+      *horizontal_moves,
+      *diagonal_moves
+    ]
     moves
   end
 
   def vertical_moves
+    moves = []
+    y_coordinates = chess_line(@y)
+    y_coordinates.each do |num|
+      moves.push([@x, num])
+    end
+    moves
   end
 
   def horizontal_moves
+    moves = []
+    x_coordinates = chess_line(@x)
+    x_coordinates.each do |num|
+      moves.push([num, @y])
+    end
+    moves
   end
 
   def diagonal_moves
+    moves = []
+    diagonal_coordinates = chess_line(@x)
+    diagonal_coordinates.each do |num|
+      moves.push([num, num])
+    end
+    moves
   end
 end
 
+first = Queen.new([0, 0], "white")
+p first.possible_moves
