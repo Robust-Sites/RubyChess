390205ffc702802ec0290712daadeb296782c9eb
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-11 16:57:08 -0400

    Writing spec file for knight.

---
+++ spec/knight_spec.rb
@@ -0,0 +1,4 @@
+require_relative '../src/pieces/knight.rb'
+
+describe Knight do
+  context ""
--- src/pieces/knight.rb
+++ src/pieces/knight.rb
@@ -2,38 +2,57 @@ require_relative 'piece'
 require_relative 'piece_helpers'
 
 class Knight < Piece
-    def initialize(coordinate, color)
-        super(coordinate, color)
-        @x = coordinate[0]
-        @y = coordinate[1]
-    end
+  attr_accessor :coordinate, :color
 
-    def possible_moves
-        moves = []
-        moves
-    end
+  def initialize(coordinate, color)
+    super(coordinate, color)
+      @x = coordinate[0]
+      @y = coordinate[1]
+  end
+
+  def possible_moves
+    moves = []
+    moves
+  end
     
-    def return_moves
-        #check confines of start values
-        x_start = @x - 3 
-        while x_start < 0
-            x_start += 1
-        end
-        y_start = @y - 3 
-        while y_start < 0
-            y_start += 1
-        end
-
-        #check confines of end values
-        x_final = @x + 3
-        while x_final > 7
-            x_final -= 1
-        end
-        y_final = @y + 3
-        while y_final > 7
-            y_final -= 1
-        end
-
-        #actual logic
+  def return_moves
+    moves = []
+    #check confines of start values
+    x_start = @x - 3 
+    while x_start < 0
+      x_start += 1
     end
+    y_start = @y - 3 
+    while y_start < 0
+      y_start += 1
+    end
+
+    #check confines of end values
+    x_final = @x + 3
+    while x_final > 7
+      x_final -= 1
+    end
+    y_final = @y + 3
+    while y_final > 7
+      y_final -= 1
+    end
+
+    #actual logic
+    while x_start < x_final
+      while y_start < y_final
+        change_in_x = x_start - @x
+        change_in_y = y_start - @y
+        absolute_value_x = change_in_x.abs()
+        absolute_value_y = change_in_y.abs()
+        moved_three_spaces = (change_in_x).abs() + (change_in_y).abs()
+        if (moved_three_spaces)
+          moves.push([x_start, y_start])
+        end
+        y_start += 1
+      end
+      x_start += 1
+    end
+  end
 end
+knight = Knight.new([0,0], 'black')
+p knight.possible_moves()
