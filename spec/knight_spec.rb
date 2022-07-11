3bb39677524442aad0f7c5c36ca377549cc681c6
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-11 17:09:20 -0400

    Finished 1st context of Knight.

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../src/pieces/knight.rb'
 
 describe Knight do
-  context ""
+  context "An instance of knight" do 
+    knight = Knight.new([0,0], "white")
+
+    it "should have a coordinate and a color" do
+      expect(knight.coordinate).not_to be_nil
+      expect(knight.color).not_to be_nil
+    end
+
+    it "can return a list of possible_moves" do
+      expect(knight.possible_moves)
+    end
+  end
+
+  
