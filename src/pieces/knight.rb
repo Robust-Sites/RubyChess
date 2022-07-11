43ada0b06dfb7298bdae52605640a673cb29983d
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-11 15:39:41 -0400

    Added initialize function and empty function of possible moves.

--- src/pieces/knight.rb
+++ src/pieces/knight.rb
@@ -1,2 +1,13 @@
 require_relative 'piece'
 require_relative 'piece_helpers'
+
+class Knight < Piece
+    def initialize(coordinate, color)
+        super(coordinate, color)
+    end
+
+    def possible_moves
+        moves = []
+        moves
+    end
+end
