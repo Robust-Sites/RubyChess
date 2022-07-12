ecb1755b1474ecb367fbb3bb329e0c0374b22f3e
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-11 20:54:45 -0400

    added initializing function

--- src/pieces/rook.rb
+++ src/pieces/rook.rb
@@ -3,4 +3,11 @@ require_relative 'piece_helpers'
 
 class Rook < Piece
   attr_accessor :coordinate, :color
+
+  def initialize(coordinate, color)
+    super(coordinate, color)
+    @x = coordinate[0]
+    @y = coordinate[1]
+  end
+  
 end
