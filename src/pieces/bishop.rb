cbf6fe535eda92ffba8424ac9069db048e3bd2ef
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-12 15:23:59 -0400

    Attempt at inverse of y = -x move function. Needs testing.

--- src/pieces/bishop.rb
+++ src/pieces/bishop.rb
@@ -31,7 +31,14 @@ class Bishop < Piece
   def y_equals_negative_x_moves
     moves = []
     x_coordinates = chess_line(@x)
-    y_coordinates = chess_line(@y)
+    y_coordinates = negative_chess_line(@y)
+    y_index = 0
+    x_coordinates.each do |num|
+      moves.push([num, y_coordinates[y_index]])
+      y_index += 1
+    end
+    moves
+  end
 end
 bishop = Bishop.new([3,3], "black")
 p bishop.possible_moves
