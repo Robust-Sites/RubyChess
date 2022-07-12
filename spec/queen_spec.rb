85b7020c61d22ca707a59389fd07cb82250d4493
jjmiranda8 <jmlivingfree@gmail.com>
2022-07-12 12:23:06 -0500

    add spec_helper_methods.rb and finish up specs for queen.rb

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../src/pieces/queen.rb'
+require_relative '../spec/spec_helper_methods.rb'
 
 describe Queen do 
   context "An instance of queen" do 
@@ -10,14 +11,35 @@ describe Queen do
     end
 
     it "can return a list of possible_moves" do 
-      expect(queen.possible_moves
+      expect(queen.possible_moves).not_to be_nil
     end
   end
 
   context "A queen's possible_moves" do
     queen = Queen.new([0,0], "white")
-    it "should not return value's that are out of bounds" do 
-      expect(queen.possible_moves).to
+
+    it "does not return value's that are out of bounds" do 
+      moveset = queen.possible_moves
+      moveset.each do |coordinate|
+        x = coordinate[0]
+        y = coordinate[1]
+        expect(x).not_to be < 0 
+        expect(x).not_to be > 7 
+        expect(y).not_to be < 0
+        expect(y).not_to be > 7
+      end 
+    end
+
+    it "returns all coordinates except its own in available chess_lines" do 
+      moveset = queen.possible_moves
+      expect(moveset).not_to include([0,0])
+    end 
+
+    it "returns coordinates for its moveset in array" do 
+      coordinates_to_test = create_coordinate_array(
+                              [0,0,0,0,0,0,0,1,2,3,4,5,6,7,1,2,3,4,5,6,7],
+                              [1,2,3,4,5,6,7,1,2,3,4,5,6,7,0,0,0,0,0,0,0])
+      assert_coordinates(queen, coordinates_to_test)
     end
   end
 end
