01336aad181f66628998b1265825db88a88d2ef1
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-12 15:04:09 -0400

    Added class and initialization for bishop.

--- src/pieces/bishop.rb
+++ src/pieces/bishop.rb
@@ -1,2 +1,11 @@
 require_relative 'piece'
 require_relative 'piece_helpers'
+
+class Bishop < Piece
+  attr_accessor :coordinate, :color
+  def initialize(coordinate, color)
+    super(coordinate, color)
+    @x = coordinate[0]
+    @y = coordinate[1]
+  end
+end
