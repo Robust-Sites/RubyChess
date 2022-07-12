bd2884b0d0995aa002cb332024bbad064b8e0e23
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-12 15:16:59 -0400

    Created bishop function. y = x moves works correctly and returns correct output.

--- src/pieces/bishop.rb
+++ src/pieces/bishop.rb
@@ -8,4 +8,25 @@ class Bishop < Piece
     @x = coordinate[0]
     @y = coordinate[1]
   end
+
+  def possible_moves
+    moves = [
+      *y_equals_x_moves
+    ]
+    moves
+  end
+
+  def y_equals_x_moves
+    moves = []
+    x_coordinates = chess_line(@x)
+    y_coordinates = chess_line(@y)
+    y_index = 0
+    x_coordinates.each do |num|
+      moves.push([num, y_coordinates[y_index]])
+      y_index += 1
+    end
+    moves
+  end
 end
+bishop = Bishop.new([3,3], "black")
+p bishop.possible_moves
