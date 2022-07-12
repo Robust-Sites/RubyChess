f0c5266c998d18f98a82d7dcb158ee676f007fd3
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-11 21:36:19 -0400

    Created first it-do pair for rook_spec.

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,1 +1,12 @@
-require '../src/pieces/queen.rb'
+require '../src/pieces/rook.rb'
+
+describe Rook do 
+  context "An instance of rook" do
+    rook = Rook.new([0,0], "white")
+
+    it "should have a coordinate and a color" do
+      expect(rook.coordinate).not_to be_nil
+      expect(rook.color).not_to be_nil
+    end
+
+    
