6a6a7caefe42c6ec9eb1fbd95f48aaa9326d1611
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-13 22:22:15 -0400

    Added coordinate and color test to bishop spec.

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -4,3 +4,6 @@ require_relative '../spec/spec_helper_methods.rb'
 describe Bishop do
   context "An instance of knight" do
     knight = Knight.new([5,5], "black")
+
+    it "should have a coordinate and a color" do
+      expect(knight.coordinate).not_to be_nil
