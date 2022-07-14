b906cc9ec84af197c845811f44c392521c90dbbc
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-13 22:25:28 -0400

    Finished 1st context for queen checking that it is instantiated correctly.

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -7,3 +7,10 @@ describe Bishop do
 
     it "should have a coordinate and a color" do
       expect(knight.coordinate).not_to be_nil
+      expect(knight.color).not_to be_nil
+    end
+
+    it "can return a list of possible_moves" do
+      expect(queen.possible_moves).not_to be_nil
+    end
+  end
