67ea069aaac438ebaca9dde9cfbc2cd487769660
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-13 21:58:02 -0400

    Created instance of bishop in spec file not tested during the creation of bishop.

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,2 +1,6 @@
 require_relative '../src/pieces/bishop.rb'
 require_relative '../spec/spec_helper_methods.rb'
+
+describe Bishop do
+  context "An instance of knight" do
+    knight = Knight.new([5,5], "black")
