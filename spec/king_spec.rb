148d26336fcc6cd231f194a0c065fb93cc5c99ee
jjmiranda8 <jmlivingfree@gmail.com>
2022-07-17 19:23:44 -0500

    clean up spec file names and add another king spec for possible_moves

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -4,20 +4,20 @@ describe King do
   context "An instance of king" do 
     king = King.new([4, 0], "white")
 
-    it "should have a coordinate" do 
+    it "has coordinate" do 
       expect(king.coordinate).not_to be_nil
     end
 
-    it "should have a color" do 
+    it "has a color" do 
       expect(king.color).not_to be_nil
     end
 
-    it "returns moveset of possible_moves" do 
+    it "has appropriate moveset of possible_moves" do 
       expect(king.possible_moves).not_to be_nil
     end
   end
 
-  context "king on edge of board" do 
+  context "edge of board" do 
     king = King.new([4, 0], "white")
 
     it "returns coordinate moveset that is not out of bounds" do 
@@ -28,12 +28,23 @@ describe King do
     end
   end
 
-  context "king in middle of board" do 
+  context "middle of board" do 
     king = King.new([4, 2], "white")
 
     it "can move 1 space in every direction" do 
       moveset = create_coordinate_array(
                   [3,3,3,4,4,5,5,5],
+                  [3,2,1,3,1,3,2,1])
+      assert_coordinates(king, moveset)
+    end
+  end
+
+  context "on [3, 2]" do 
+    king = King.new([3, 2], "white")
+
+    it "has appropriate possible_moves" do 
+      moveset = create_coordinate_array(
+                  [2,2,2,3,3,4,4,4],
                   [3,2,1,3,1,3,2,1])
       assert_coordinates(king, moveset)
     end
