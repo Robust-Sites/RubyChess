72428b21f3357b2a42b886304174711d870976db
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-18 17:25:50 -0400

    Created intialization that takes necessary info of a turn.

--- src/turn.rb
+++ src/turn.rb
@@ -1,12 +1,9 @@
 class Turn
   attr_accessor: :turn_number, :prev, :next
-  def initialize
+  def initialize(piece, move_from, move_to, captured_piece=False, captured_piece=nil) 
+    @piece = piece
     @next = nil
     @prev = nil 
     @turn_number = 0
   end
-
-
-
-
-end 
+end
