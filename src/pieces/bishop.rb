8ce91ef64f167569d30b23e5e1f5f1f89b6adf2d
jjmiranda8 <jmlivingfree@gmail.com>
2022-07-17 19:31:46 -0500

    DRY up code by move y_equals_x_moves and y_equals_negative_x_moves to piece_helpers

--- src/pieces/bishop.rb
+++ src/pieces/bishop.rb
@@ -17,32 +17,4 @@ class Bishop < Piece
     moves
   end
 
-  def y_equals_x_moves
-    moves = []
-    index = 0
-    while index <= 7
-      difference_between_x = @x - index
-      y_value = @y - difference_between_x
-      x_value_not_coordinate = (index != @x)
-      if y_value >= 0 && x_value_not_coordinate
-        moves.push([index, y_value])
-      end
-      index += 1
-    end
-    moves
-  end
-
-  def y_equals_negative_x_moves
-    moves = []
-    range = @x + @y
-    x_coordinates = chess_line(@x)
-    x_coordinates.each do |num|
-      y_coordinate = range - num
-      y_in_range =y_coordinate >= 0 && y_coordinate <= 7
-      if y_in_range
-        moves.push([num, y_coordinate])
-      end
-    end
-    moves
-  end
 end
