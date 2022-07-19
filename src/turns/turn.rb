70f6a1397f994a6e3a8bf664304d12d96de0b28c
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-19 14:23:22 -0400

    Currently figuring how to implement Turn as nodes into a turnlist. I will create a node class that will hold a turn to implement into Turnlist for now.

--- src/turns/turn.rb
+++ src/turns/turn.rb
@@ -1,5 +1,11 @@
+
+
+
+
+
+
 class Turn
-  attr_accessor: :piece, :move_from, :move_to, :captured_piece, :next, :prev
+  attr_accessor :piece, :move_from, :move_to, :captured_piece, :next, :prev
   def initialize(piece, move_from, move_to, captured_piece=nil) 
     @piece = piece
     @move_from = move_from
--- src/turns/turn_list.rb
+++ src/turns/turn_list.rb
@@ -8,3 +8,16 @@ class TurnList
     @prev = nil
     @move_number = 0
   end
+
+  def push(piece, move_from, move_to, captured_piece=nil)
+    if captured_piece != nil
+      new_turn = Turn.new(piece, move_from, move_to, captured_piece)
+    else
+      new_turn = Turn.new(piece, move_from, move_to)
+    end
+  end
+end
+
+turnlist = TurnList.new
+turnlist.push("black", "A4", "B4")
+p turnlist.captured_piece()
