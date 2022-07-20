79e0bbea94e41d104fb2a5546250be85f37047d6
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-19 23:47:29 -0400

    Created pop to remove function from the end of the linked list.

--- src/turns/turn_list.rb
+++ src/turns/turn_list.rb
@@ -23,6 +23,30 @@ class TurnList #This is a double linked list.
     @length += 1
     return self
   end
+  
+  def pop
+    return nil if !@head
+    old_tail = @tail
+    if @length == 1
+      @head = nil
+      @tail = nil
+    else
+      @tail = old_tail.prev
+      @tail.next = nil
+      old_tail.prev = nil
+    end
+    @length -= 1
+    return old_tail
+  end
+  
+  rescue => exception
+    
+  else
+    
+  ensure
+    
+  end
+  end
 
 end
 
