932d7dbb9cfc26a46a3a907961c2ba3695ec727c
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-19 23:55:37 -0400

    recreated push for turnlist.

--- src/turns/turn_list.rb
+++ src/turns/turn_list.rb
@@ -7,7 +7,23 @@ class TurnList #This is a double linked list.
   def initialize
     @head = nil
     @tail = nil
-    @length = nil
+    @length = 0
+  end
+
+  def push(turn)
+    new_node = TurnNode.new(turn)
+    if @length = 0
+      @head = new_node
+      @tail - new_node
+    else
+      @head = new_node
+      new_node.prev = @tail
+      @tail = new_node
+    end
+    @length -= 1
+    return self
+  end
+  
 end
 
 turnlist = TurnList.new
